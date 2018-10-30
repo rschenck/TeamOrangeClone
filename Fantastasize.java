8c0be68347a186abb4f6699516654b9ad7106e6b
Ryan Schenck <rschenck@Ryans-MacBook-Pro-2.local>
2018-10-30 14:04:49 -0400

    Ready Ready

--- Fantastasize.java
+++ Fantastasize.java
@@ -6,19 +6,20 @@ import java.io.IOException;
 import java.io.FileWriter;
 
 class Cell{   
-    static int[] maxmin={10,50};//high and low of cell cycle values
+    static int[] maxmin={10,40};//high and low of cell cycle values
     static Random generator = new Random(); //random number generator
     static int neoMutRate = 100;//mut 1/1000
     static int mutRate = 1000;
 
     int cellCycleLength;
-	int positionInCellCycle=0;
+	int positionInCellCycle;
 	boolean brafRes;
 	boolean PDL1;
 	boolean pR;
 	int[] neoAntigenLoad = new int[Fantastasize.numNA];
     
 	public Cell(){//constructor
+	    this.positionInCellCycle=0;
         this.cellCycleLength=getCellCycleLength();
         this.neoAntigenLoad=getNeoAntigenLoad();
         this.brafRes = getBRAFresStat();
@@ -30,7 +31,8 @@ class Cell{
         this.PDL1=false;
         this.brafRes=false;
         this.pR=false;
-        this.neoAntigenLoad[Cell.generator.nextInt(Fantastasize.numNA)]=1;//
+        this.neoAntigenLoad[Cell.generator.nextInt(Fantastasize.numNA)]=1;
+        this.cellCycleLength=maxmin[1];
     }
     
 	public void advance() {
@@ -59,14 +61,14 @@ class Cell{
 	
 	public void reset(){
 		this.positionInCellCycle=0;
-		if(generator.nextInt(neoMutRate)==0){//random roll
+		if(generator.nextInt(neoMutRate)==0){//random roll - neoAntigen accumulation
 			int newNA = generator.nextInt(Fantastasize.numNA);
 			this.neoAntigenLoad[newNA]+=1;
 			Fantastasize.totNAL[newNA]+=1;
 			Fantastasize.TCR[newNA]+=1;
 //			System.out.println("hi");
 		}
-		if(generator.nextInt(mutRate)==0){
+		if(generator.nextInt(mutRate)==0){//mutation rate
 		    int whichMut = generator.nextInt(3);
 		    if(whichMut==0){
                 this.cellCycleLength=maxmin[0];
